7a74db748c8c640c26e905d7af1f57ac7e8d7698
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2020-11-30 16:49:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -56,14 +56,14 @@ async def on_timeSignal():
                 await channel.send("13時です！早く仕事に戻ってください！")
 
         elif dt_now.hour == 16:
-            if dt_now.minute == 50:
+            if dt_now.minute == 52:
                 voice = await discord.VoiceChannel.connect(client.get_channel(vChannelID))
                 await channel.send("退勤！！！！！！！！！！！！！")
                 audioSource = discord.FFmpegPCMAudio("18zi.wav")
                 voice.play(audioSource)
                 time.sleep(10)
                 
-                await voice.disconnect(
+                await voice.disconnect()
                 await channel.send("まもなく夕会のお時間です。日報の提出をお願いします。")
 
         elif dt_now.hour == 18:
