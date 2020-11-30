f0b16d05478ddfaeed760e7a7478f4606445e163
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2020-11-30 16:30:10 +0900

    音声流すテスト

--- discordbot.py
+++ discordbot.py
@@ -57,11 +57,24 @@ async def on_timeSignal():
 
         elif dt_now.hour == 16:
             if dt_now.minute == 50:
-                await channel.send("まもなく夕会のお時間です。日報の提出をお願いします。")
+                voice = await discord.VoiceChannel.connect(client.get_channel(vChannelID))
+                await channel.send("退勤！！！！！！！！！！！！！")
+                audioSource = discord.FFmpegPCMAudio("18zi.wav")
+                voice.play(audioSource)
+                time.sleep(10)
+                
+                await voice.disconnect()
+                # await channel.send("まもなく夕会のお時間です。日報の提出をお願いします。")
 
         elif dt_now.hour == 18:
             if dt_now.minute == 0:
+                voice = await discord.VoiceChannel.connect(client.get_channel(vChannelID))
                 await channel.send("退勤！！！！！！！！！！！！！")
+                audioSource = discord.FFmpegPCMAudio("18zi.wav")
+                voice.play(audioSource)
+                time.sleep(10)
+                
+                await voice.disconnect()
 
 on_timeSignal.start()
 bot.run(token)
