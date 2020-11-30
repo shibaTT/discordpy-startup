20bb83a91ad7392aaf3f55bd4738256d49c1220e
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2020-11-30 18:40:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,60 +1,89 @@
 import discord
+import time
+import datetime
+import asyncio
+from discord.ext import tasks  # taskというライブラリをdiscord.extという名前にしてる？
 from discord.ext import commands
-
 import os
 
-bot = commands.Bot(command_prefix="$")
+bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-if not discord.opus.is_loaded():
-    discord.opus.load_opus("heroku-buildpack-libopus")
+## Global settings ##
+client = discord.Client()
+channelID = 758983784963637251
+vChannelID = 758983784963637252
 
-@bot.command(aliases=["connect","summon"]) #connectやsummonでも呼び出せる
-async def join(ctx):
-    """Botをボイスチャンネルに入室させます。"""
-    voice_state = ctx.author.voice
+#if not discord.opus.is_loaded():
+#もし未ロードだったら
+#discord.opus.load_opus("heroku-buildpack-libopus")
 
-    if (not voice_state) or (not voice_state.channel):
-        await ctx.send("先にボイスチャンネルに入っている必要があります。")
-        return
+## 初期設定 ##
+@bot.event
+async def on_ready():
+    channel = client.get_channel(channelID)
+    print('Logged in as')
+    # print(client.user.name)
+    # print(client.user.id)
+    print('------')
+    game = discord.Game("時報です")
+    await client.change_presence(activity=game)
+    channel.send("うんちbotは死にました")
 
-    channel = voice_state.channel
+"""
+@tasks.loop(seconds=60)
+async def on_timeSignal():
+    dt_now = datetime.datetime.now()
+    await client.wait_until_ready()  # on_ready内でget_channelしないとエラー出るので、その対策
+    channel = client.get_channel(channelID)  # チャンネルの対象IDからチャンネル情報を取得
+    # ボイスチャンネルにジョイン、部屋情報をvoiceに格納
+    # await channel.send("on_timeSignalのループ始動")
+    # ボイスチャンネルの参考元:https://qiita.com/sizumita/items/cafd00fe3e114d834ce3
+    # ↑情報古いので関数名とクラス名変わってますです
 
-    await channel.connect()
-    print("connected to:",channel.name)
+    # 月曜～金曜の間で
+    if dt_now.weekday() >= 0 and dt_now.weekday() < 5:
 
+        # 8:50になったら
+        if dt_now.hour == 8:
+            if dt_now.minute == 50:
+                await channel.send("8時50分になりました！「出社」をお忘れなく！")
 
-@bot.command(aliases=["disconnect","bye"])
-async def leave(ctx):
-    """Botをボイスチャンネルから切断します。"""
-    voice_client = ctx.message.guild.voice_client
+        elif dt_now.hour == 12:
+            if dt_now.minute == 0:
+                await channel.send("12時です！お昼ごはんを食べましょう")
 
-    if not voice_client:
-        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
-        return
+        elif dt_now.hour == 13:
+            if dt_now.minute == 0:
+                await channel.send("13時です！早く仕事に戻ってください！")
 
-    await voice_client.disconnect()
-    await ctx.send("ボイスチャンネルから切断しました。")
+        elif dt_now.hour == 16:
+            if dt_now.minute == 50:
+                
+                voice = await discord.VoiceChannel.connect(client.get_channel(vChannelID))
+                await channel.send("退勤！！！！！！！！！！！！！")
+                audioSource = discord.FFmpegPCMAudio("18zi.wav")
+                voice.play(audioSource)
+                time.sleep(10)
+                
+                await voice.disconnect()
+                
+                await channel.send("まもなく夕会のお時間です。日報の提出をお願いします。")
 
+        elif dt_now.hour == 18:
+            if dt_now.minute == 0:
+                
+                voice = await discord.VoiceChannel.connect(client.get_channel(vChannelID))
+                await channel.send("退勤！！！！！！！！！！！！！")
+                audioSource = discord.FFmpegPCMAudio("18zi.wav")
+                voice.play(audioSource)
+                time.sleep(10)
+                
+                await voice.disconnect()
+                
+                
+                await channel.send("退勤！！！！！！！！！！！！！")
 
-@bot.command()
-async def play(ctx):
-    """指定された音声ファイルを流します。"""
-    voice_client = ctx.message.guild.voice_client
-
-    if not voice_client:
-        await ctx.send("Botはこのサーバーのボイスチャンネルに参加していません。")
-        return
-
-    if not ctx.message.attachments:
-        await ctx.send("ファイルが添付されていません。")
-        return
-
-    await ctx.message.attachments[0].save("tmp.mp3")
-
-    ffmpeg_audio_source = discord.FFmpegPCMAudio("tmp.mp3")
-    voice_client.play(ffmpeg_audio_source)
-
-    await ctx.send("再生しました。")
-
+on_timeSignal.start()
+"""
 bot.run(token)
